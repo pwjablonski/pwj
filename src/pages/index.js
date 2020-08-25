1f4bbabec7fcf7536c42ca7846ffa777cbf5ef8e
Peter Jablonski <pwjablonski@gmail.com>
2020-08-25 15:39:24 -0500

    seperate projects

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ import scrollTo from 'gatsby-plugin-smoothscroll';
 
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPaperPlane, faChevronDown } from '@fortawesome/free-solid-svg-icons'
+import { faPaperPlane, faChevronDown, faChevronUp} from '@fortawesome/free-solid-svg-icons'
 import { faFileAlt } from '@fortawesome/free-regular-svg-icons'
 import { faGithub, faLinkedin, faCodepen } from '@fortawesome/free-brands-svg-icons'
 
@@ -41,36 +41,56 @@ const IndexPage = ({location, data}) => {
               <FontAwesomeIcon className={style.icon} icon={faFileAlt} />
             </Link>
           </div>
-          <FontAwesomeIcon className={style.chevron} icon={faChevronDown} onClick={() => scrollTo('#projects')}/>
+        </div>
+        <div className={style.chevron}>
+          <FontAwesomeIcon  icon={faChevronDown} onClick={() => scrollTo('#freelance')}/>
         </div>
       </section>
-      <section className={style.page} id="projects">
-        <h1 className={style.page_title}>Freelance</h1>
+      <section className={style.page} id="freelance">
         <div className={style.projects}>
-        {
-          projects.map((project) => {
-            return project.node.frontmatter.category === "Freelance" && <Project data={project.node.frontmatter} key={project.node.frontmatter.title} />;
-          })
-        }
+          <h1 className={style.page_title}>Freelance</h1>
+          <div className={style.projects_grid}>
+          {
+            projects.map((project) => {
+              return project.node.frontmatter.category === "Freelance" && <Project data={project.node.frontmatter} key={project.node.frontmatter.title} />;
+            })
+          }
+          </div>
         </div>
-        <h1 className={style.page_title}>Open Source</h1>
-        <div className={style.projects}>
-        {
-          projects.map((project) => {
-            return project.node.frontmatter.category === "Open Source" && <Project data={project.node.frontmatter} key={project.node.frontmatter.title} />;
-          })
-        }
-        </div>
-        <h1 className={style.page_title}>Personal Projects</h1>
-        <div className={style.projects}>
-        {
-          projects.map((project) => {
-            return project.node.frontmatter.category === "Personal" && <Project data={project.node.frontmatter} key={project.node.frontmatter.title} />;
-          })
-        }
+        <div className={style.chevron}>
+          <FontAwesomeIcon className={style.chevron} icon={faChevronDown} onClick={() => scrollTo('#open-source')}/>
         </div>
       </section>
-  
+      <section className={style.page} id="open-source">
+        <div className={style.projects}>
+          <h1 className={style.page_title}>Open Source</h1>
+          <div className={style.projects_grid}>
+          {
+            projects.map((project) => {
+              return project.node.frontmatter.category === "Open Source" && <Project data={project.node.frontmatter} key={project.node.frontmatter.title} />;
+            })
+          }
+          </div>
+        </div>
+        <div className={style.chevron}>
+          <FontAwesomeIcon className={style.chevron} icon={faChevronDown} onClick={() => scrollTo('#personal')}/>
+        </div>
+      </section>
+      <section className={style.page} id="personal">
+        <div className={style.projects}>
+          <h1 className={style.page_title}>Personal Projects</h1>
+          <div className={style.projects_grid}>
+          {
+            projects.map((project) => {
+              return project.node.frontmatter.category === "Personal" && <Project data={project.node.frontmatter} key={project.node.frontmatter.title} />;
+            })
+          }
+          </div>
+        </div>
+        <div className={style.chevron}>
+          <FontAwesomeIcon className={style.chevron} icon={faChevronUp} onClick={() => scrollTo('#intro')}/>
+        </div>
+      </section>
     </Layout>
   )
 }
